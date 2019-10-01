6296992d2889e0c320727d3361a85e0a91c70b49
Paul Inventado <pinventado@gmail.com>
2019-10-01 16:25:54 -0700

    Initial commit

---
+++ prob04/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+  int length, width;
+  std::cout << "Please enter the length: ";
+  std::cin >> length;
+  std::cout << "Please enter the width: ";
+  std::cin >> width;
+  // Add the code here to call the appropriate functions
+  // and  display the correctly formatted output on the
+  // screen.
+  return 0;
+}
---
+++ prob05/main.cpp
@@ -0,0 +1,16 @@
+#include "most_sig_digit.hpp"
+#include <iostream>
+
+int main() {
+  int input = 0, most_significant = 0;
+  std::cout << "Please enter an integer, this program will produce it's more "
+               "significant digit: ";
+  std::cin >> input;
+
+  // Add the code here to store the most significant digit into
+  // the most_significant variable
+
+  std::cout << "The most significant digit is " << most_significant << "\n";
+
+  return 0;
+}
